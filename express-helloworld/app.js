714602a84f11e317584cef865059c23d03e3a6c2
Siddhanth Panikar <143732252+rafig0@users.noreply.github.com>
2025-01-24 10:49:56 +0530

    added example

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
 });
 
 app.listen(8080, function () {
-  console.log(' app listening on port 8080!');
+  console.log('example app listening on port 8080!');
 });
 
